eac070773d366cfb8ec66fe609c4674679b76caf
J0shcodes <mosesjoshua350@gmail.com>
2022-09-10 23:22:01 +0100

    Removed environment variables

--- next.config.js
+++ next.config.js
@@ -1,30 +1,9 @@
 /** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const nextConfig = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      swcMinify: true,
-      env: {
-        mongodb_username: "J0shcodes",
-        mongodb_password: "kNX4n8pZXoEUitTm",
-        mongodb_cluster: "myblog-cluster",
-        mongodb_database: "blog-site-dev",
-      },
-    };
-  }
-
-  return {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-      mongodb_username: "J0shcodes",
-      mongodb_password: "kNX4n8pZXoEUitTm",
-      mongodb_cluster: "myblog-cluster",
-      mongodb_database: "blog-site",
-    },
-  };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
